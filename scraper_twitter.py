f8159b515452bb98161e0ccb17cd19bbdae45d0e
Fabricio Ramirez <wramirez995@gmail.com>
2021-11-25 01:29:12 -0400

    Twitter works

--- scraper_twitter.py
+++ scraper_twitter.py
@@ -21,26 +21,29 @@ try:
     driver.get(current_url)
     #os.system("pause")
     time.sleep(2)
+    result = [] 
     for item in range(10):
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         time.sleep(5)
-    content = driver.page_source
+        content = driver.page_source
+
+        for tweets in BeautifulSoup(content, 'html.parser').find_all('article'):
+            dm = {}
+            dm["type"] = 'tw_post'
+            dm["source"] = current_url
+            for author in tweets.find('div', class_='css-901oao r-1awozwy r-1fmj7o5 r-6koalj r-37j5jr r-a023e6 r-b88u0q r-rjixqe r-bcqeeo r-1udh08x r-3s2u2q r-qvutc0'):
+                dm["author"] = utils.clean_soup(author)
+            for date in tweets.find_all('time'):
+                if date.has_attr('datetime'):
+                    dm["date"] = date['datetime']
+            for text in tweets.find_all('div', class_='css-901oao r-1fmj7o5 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0'):
+                dm['text'] = utils.clean_soup(text)
+            result.append(dm)
 except:
     print('webdriver timeout... ')
     content = ''
 
 # close the driver
-
-result = [] 
-for t in BeautifulSoup(content, "html.parser").find_all('span', class_='css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0'):
-
-    dm = {}
-    dm["type"] = 'tw_post'
-    dm["source"] = current_url
-    dm["text"] = utils.clean_soup(t)
-
-    result.append(dm)
-
 
 
 
